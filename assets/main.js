ef215a3c34db1e0df5bfc5907b32cef027dfc21a
Umbeee <umberto.nesta@hotmail.com>
2023-03-21 16:23:47 +0100

    esercizio completo

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,8 @@
-// variabili
-
 //chiedere input nome cognome colore preferito
-
+let nome = prompt("Inserire nome");
+let cognome = prompt("Inserire cognome");
+let colore = prompt("Inserire colore preferito");
 //output nome+cognome+colore+21
+
+console.log( nome );
+document.getElementById('passwordDiv').innerHTML = nome + cognome + colore + 21;
